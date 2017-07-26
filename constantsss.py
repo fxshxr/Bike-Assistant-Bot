b8c0242b1627f83e48cf87e337c97a1e8826f277
fxshxr <igor.lytkin@gmail.com>
2017-07-26 13:41:59 +0300

    Add files via upload

--- constantsss.py
+++ constantsss.py
@@ -1,3 +1,8 @@
 token = '430436767:AAFjuew233QkRK9wXVThJyK62zuEi2YPVbM'
-template_sticker_id = 'BQADAgADBwcAAvvo_wsmviDkHCihngI'
-template_sticker_id7 = 'CAADAgADjgAD9zVWDj0DjB4vCa0YAg'
+template_sticker_idpizd = 'BQADAgADBwcAAvvo_wsmviDkHCihngI'
+template_sticker_idejje = 'CAADAgADjwAD9zVWDtNJ7ZEUhQ3ZAg'
+template_sticker_idandr = 'CAADAgADjgAD9zVWDj0DjB4vCa0YAg'
+template_sticker_idkota = 'CAADAgADigAD9zVWDjiAGqQON0toAg'
+template_sticker_idfish = 'CAADAgADjgMAAhpchATmYPilXm6zWgI'
+template_sticker_idvas ='CAADAgADnwAD9zVWDsHn9V_itDyBAg'
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 import telebot
 import constantsss
 import dialogs
@@ -11,20 +12,31 @@ print(bot.get_me())
 
 def reply_message(message):
 
-    if "а?"  in message.text.lower():
+    if ", а?"  in message.text.lower():
+            answer = "хуй на"
+            (message, answer)
+            bot.send_message(message.chat.id,answer)
+    elif ",а?"  in message.text.lower():
             answer = "хуй на"
             (message, answer)
             bot.send_message(message.chat.id,answer)
     elif "пиздуль" in message.text.lower():
-        bot.send_sticker(message.chat.id, constantsss.template_sticker_id)
-    elif "!бутылка" in message.text.lower():
+        bot.send_sticker(message.chat.id, constantsss.template_sticker_idpizd)
+    elif "я русский" in message.text.lower():
         bot.reply_to(message,  random.choice(dialogs.but))
     elif "!чашка" in message.text.lower():
         bot.reply_to(message, random.choice(dialogs.chuska))
-    elif "рейт" in message.text.lower():
+    elif ",рейт" in message.text.lower():
+        bot.reply_to(message, random.choice(dialogs.reit))
+    elif ", рейт" in message.text.lower():
+        bot.reply_to(message, random.choice(dialogs.reit))
+    elif "рейт ми" in message.text.lower():
+        bot.reply_to(message, random.choice(dialogs.reit))
+    elif " рейт" in message.text.lower():
+        bot.reply_to(message, random.choice(dialogs.reit))
+    elif "Рейт " in message.text.title():
         bot.reply_to(message, random.choice(dialogs.reit))
     elif "харитон" in message.text.lower():
-
         bot.reply_to(message, random.choice(dialogs.khar))
     elif "!ролл" in message.text.lower():
         bot.reply_to(message, random.choice(dialogs.roll))
@@ -44,12 +56,29 @@ def reply_message(message):
        bot.reply_to(message, random.choice(dialogs.eeee))
     elif "плюсики" in message.text.lower():
        bot.reply_to(message, random.choice(dialogs.plc))
-    elif "/help_bike" in message.text.lower():
+    elif "!хелп" in message.text.lower():
        bot.reply_to(message, random.choice(dialogs.coms))
     elif "андрей"  in message.text.lower():
-        bot.send_sticker(message.chat.id, constantsss.template_sticker_id7)
-    elif "стрида" and "страйда" in message.text.lower():
+        bot.send_sticker(message.chat.id, constantsss.template_sticker_idandr)
+    elif "стрида" in message.text.lower():
         bot.reply_to(message, random.choice(dialogs.str))
+    elif "ежжи" in message.text.lower():
+        bot.send_sticker(message.chat.id, constantsss.template_sticker_idejje)
+    elif "фишер-пидор" in message.text.lower():
+        bot.send_sticker(message.chat.id, constantsss.template_sticker_idfish)
+    elif "пидора ответ" in message.text.lower():
+        bot.reply_to(message,  random.choice(dialogs.pidor))
+    elif "я видел" in message.text.lower():
+        bot.send_sticker(message.chat.id, constantsss.template_sticker_idvas)
+    elif "!рулетка" in message.text.lower():
+        bot.reply_to(message,  random.choice(dialogs.russrullet))
+
+
+
+
+
+
+
 
 
 
